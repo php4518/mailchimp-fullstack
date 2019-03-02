ff46b72d723f861563c7bed0eeb2de3cbc18170e
php4518 <phpatel.4518@gmail.com>
2019-03-02 23:01:35 +0530

    added port

--- src/server/index.js
+++ src/server/index.js
@@ -10,4 +10,4 @@ app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: 'User' }));
 app.all('/api/mailchimp/*', mailchimpRequest);
 
-app.listen(process.env.PORT || 9005, () => console.log(`Listening on port ${process.env.PORT || 9005}!`));
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
